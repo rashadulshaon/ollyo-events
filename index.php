3bb5813d8d1a6603f91df05d40eaf84e47c8d345
Shaon <helloshaon@protonmail.com>
2025-02-02 09:22:12 +0600

    Add dump_and_die function

--- index.php
+++ index.php
@@ -3,3 +3,18 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 (new App\Kernel());
+
+if (!function_exists('dump_and_die')) {
+    function dump_and_die(...$vars)
+    {
+        header('Content-Type: text/html');
+
+        foreach ($vars as $var) {
+            echo '<pre>';
+            var_dump($var);
+            echo '</pre>';
+        }
+
+        exit;
+    }
+}
